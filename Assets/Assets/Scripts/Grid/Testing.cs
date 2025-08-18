b96ba2fc3512e18e318f73d7c76ace3a5036c127
Boerming933 <matiasdelprato@gmail.com>
2025-08-18 20:24:37 -0300

    Inicial Grillas

---
+++ Assets/Assets/Scripts/Grid/Testing.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using LibraryCodes;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = new Grid(44, 44, 5f, new Vector3(-110f, -110f));
+    }
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    grid.SetValue(UtilityMovement.GetMouseWorldPosition(), 56);
+        //}
+        
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //   Debug.Log(grid.GetValue(UtilityMovement.GetMouseWorldPosition()));
+        //}
+    }
+}
---
+++ Assets/Assets/Scripts/Library/Utility.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using LibraryCodes;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace LibraryCodes
+{
+    public struct UtilityMovement
+    {
+        //Posicion actual del mouse
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
+        //Creacion letras para grillas
+
+        public static TextMesh CreateWorldText(string text, Transform parent, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor)
+        {
+            GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+            Transform transform = gameObject.transform;
+            transform.SetParent(parent, false);
+            transform.localPosition = localPosition;
+            TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+            textMesh.anchor = textAnchor;
+            textMesh.text = text;
+            textMesh.fontSize = fontSize;
+            textMesh.color = color;
+            return textMesh;
+        }
+    }
+
+    public struct UtilityAttacks
+    {
+
+    }
+
+}
