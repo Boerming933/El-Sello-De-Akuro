f478d4769b6ce5909f7cf48674bccdaf0cb2e093
Boerming933 <matiasdelprato@gmail.com>
2025-08-30 20:21:04 -0300

    Vaporeon

--- Assets/Assets/Scripts/Grid/MouseController.cs
+++ Assets/Assets/Scripts/Grid/MouseController.cs
@@ -1,121 +1,143 @@
 using System.Collections.Generic;
-using System.Linq;
-using finished3;
-using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using System.Linq;
 
-public class MouseControler : MonoBehaviour
+namespace finished3
 {
-    public GameObject characterPrefab;
-    private List<OverlayTile> path = new List<OverlayTile>();
-    private List<OverlayTile> inRangeTiles = new List<OverlayTile>();
-
-    private CharacterInfo character;
-    private PathFinder pathFinder;
-    private RangeFinder rangeFinder;
-
-    public float speed;
-    public int movement;
-    private bool isMoving = false;
-
-
-    private void Start()
+    public class MouseController : MonoBehaviour
     {
-        pathFinder = new PathFinder();
-        rangeFinder = new RangeFinder();
-        character = GetComponent<CharacterInfo>();
-    }
+        public float speed;
+        public int steps;
+        public GameObject characterPrefab;
+        public CharacterInfo character;
+        OverlayTile Active;
 
-    void Update()
-    {
-        var focusedTileHit = GetFocusedOnTile();
+        private PathFinder pathFinder;
+        private RangeFinder rangeFinder;
+        private List<OverlayTile> path;
+        private List<OverlayTile> rangeFinderTiles;
+        private bool isMoving;
 
-        if (focusedTileHit.HasValue)
+        private void Start()
         {
-            OverlayTile overlayTile = focusedTileHit.Value.collider.gameObject.GetComponent<OverlayTile>();
-            transform.position = overlayTile.transform.position;
-            gameObject.GetComponent<SpriteRenderer>().sortingOrder = overlayTile.GetComponent<SpriteRenderer>().sortingOrder;
+            pathFinder = new PathFinder();
+            rangeFinder = new RangeFinder();
 
-            if (inRangeTiles.Contains(overlayTile) && !isMoving)
-            {
-                path = pathFinder.FindPath(character.activeTile, overlayTile, inRangeTiles);
-            }
-            
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (character == null)
-                {
-                    //PositionCharacterOnTile(overlayTile);
-                    //GetInRangeTiles();
-                }
-                else
-                {
-                    isMoving = true;
-                }
-            }
-        }
-
-        if (path.Count > 0)
-        {
-            MoveAlongPath();
-        }
-    }
-
-    private void GetInRangeTiles()
-    {
-        foreach (var item in inRangeTiles)
-        {
-            item.HideTile();
-        }
-        
-        inRangeTiles = rangeFinder.GetTilesInRange(character.activeTile, movement);
-
-        foreach (var item in inRangeTiles)
-        {
-            item.ShowTile();
-        }
-    }
-
-    private void MoveAlongPath()
-    {
-        var stop = speed * Time.deltaTime;
-
-        var zIndex = path[0].transform.position.z;
-        character.transform.position = Vector2.MoveTowards(character.transform.position, path[0].transform.position, stop);
-        character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, zIndex);
-
-        if (Vector2.Distance(character.transform.position, path[0].transform.position) < 0.0001f)
-        {
-            PositionCharacterOnTile(path[0]);
-            path.RemoveAt(0);
-        }
-
-        if (path.Count == 0)
-        {
-            GetInRangeTiles();
+            path = new List<OverlayTile>();
             isMoving = false;
+            rangeFinderTiles = new List<OverlayTile>();
+            Debug.Log(character);
         }
-    }
 
-    public RaycastHit2D? GetFocusedOnTile()
-    {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-        RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos2D, Vector2.zero);
-
-        if (hits.Length > 0)
+        void Update()
         {
-            return hits.OrderByDescending(i => i.collider.transform.position.z).First();
-        }
-        return null;
-    }
+            RaycastHit2D? hit = GetFocusedOnTile();
 
-    private void PositionCharacterOnTile(OverlayTile tile)
-    {
-        character.transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y + 0.000001f, tile.transform.position.z);
-        character.GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
-        character.activeTile = tile;
+            if (hit.HasValue)
+            {
+                OverlayTile tile = hit.Value.collider.gameObject.GetComponent<OverlayTile>();
+                transform.position = tile.transform.position;
+                var active = ActiveTile();
+                Active = active.Value.collider.GetComponent<OverlayTile>();
+                PositionCharacterOnLine(Active);
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = tile.transform.GetComponent<SpriteRenderer>().sortingOrder;
+
+                if (rangeFinderTiles.Contains(tile) && !isMoving)
+                {
+                    path = pathFinder.FindPath(Active, tile, rangeFinderTiles);
+                }
+                if (character != null && Active != null)
+                {
+                    GetInRangeTiles(Active, steps);
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    tile.ShowTile();
+
+                    if (character == null)
+                    {
+                        character = Instantiate(characterPrefab).GetComponent<CharacterInfo>();
+                    }
+                    else
+                    {
+                        isMoving = true;
+                        tile.gameObject.GetComponent<OverlayTile>().HideTile();
+                    }
+                }
+            }
+
+            if (path.Count > 0 && isMoving)
+            {
+                MoveAlongPath();
+            }
+        }
+
+        private void MoveAlongPath()
+        {
+            var step = speed * Time.deltaTime;
+
+            float zIndex = path[0].transform.position.z;
+            character.transform.position = Vector2.MoveTowards(character.transform.position, path[0].transform.position, step);
+            character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, zIndex);
+
+            if (Vector2.Distance(character.transform.position, path[0].transform.position) < 0.00001f)
+            {
+                PositionCharacterOnLine(path[0]);
+                path.RemoveAt(0);
+            }
+
+            if (path.Count == 0)
+            {
+                GetInRangeTiles(Active, steps);
+                isMoving = false;
+            }
+
+        }
+
+        public RaycastHit2D? ActiveTile()
+        {
+            Vector2 origin = new Vector2(character.transform.position.x, character.transform.position.y);
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(origin, Vector2.zero);
+
+            if (hits.Length > 0)
+            {
+                return hits.OrderByDescending(i => i.collider.transform.position.z).First();
+            }
+            return null;
+        }
+
+        private void PositionCharacterOnLine(OverlayTile tile)
+        {
+            character.transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y + 0.0001f, tile.transform.position.z-1);
+            character.GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
+            Active = tile;
+        }
+
+        private static RaycastHit2D? GetFocusedOnTile()
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos2D, Vector2.zero);
+
+            if (hits.Length > 0)
+            {
+                return hits.OrderByDescending(i => i.collider.transform.position.z).First();
+            }
+
+            return null;
+        }
+
+        public void GetInRangeTiles(OverlayTile active, int movement)
+        {
+            rangeFinderTiles = rangeFinder.GetTilesInRange(active, movement);
+
+            foreach (var item in rangeFinderTiles)
+            {
+                item.ShowTile();
+            }
+        }
     }
 }
